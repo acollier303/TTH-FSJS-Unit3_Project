1fd1c5eac55d79e9db3f3fbfdfc941cd7ab29efd
Adrian Collier <30817514+acollier303@users.noreply.github.com>
2020-04-27 16:38:21 -0600

    initial code setup

---
+++ js/script.js
@@ -0,0 +1,13 @@
+/******************************************
+Treehouse Techdegree:
+FSJS project 3 - Interactive Form
+Adrian Collier
+******************************************/
+
+//Focus on first text field on load
+document.getElementById('name').focus();
+
+//Job role other
+document.getElementById('other-title').style.display='none';
+
+

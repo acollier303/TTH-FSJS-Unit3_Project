21e89ed9b98e29faaf1c74384f0b63251b69eb90
Adrian Collier <30817514+acollier303@users.noreply.github.com>
2020-04-28 11:12:31 -0600

    T-shirt options: wip

--- js/script.js
+++ js/script.js
@@ -7,21 +7,33 @@ Adrian Collier
 //Focus on first text field on load
 document.getElementById('name').focus();
 
-//Job role other
+/***
+Job Role Section
+***/
 document.getElementById('other-title').style.display='none';
 
-//*** T-Shirt ***
+
+/*** 
+T-Shirt Section
+***/
 
 //Hide 'Select Theme' option from 'design' selection
 const designSelect = document.querySelector('#design');
-console.log(designSelect.options[0]);
-
 designSelect.options[0].hidden = 'true';
 
 //Update the “Color” field to read “Please select a T-shirt theme”
 const colorSelect = document.querySelector('#color');
+for (let i=0; i<colorSelect.length; i+=1){
+    colorSelect.options[i].hidden = 'true'; 
+}
 
-colorSelect.options.add('Please select a T-shirt theme', 0);
+//console.log(colorSelect.length);
+const defaultColor = document.createElement('option');
+defaultColor.text = 'Please select a T-shirt theme';
+colorSelect.options.add(defaultColor, 0);
+
+//console.log(colorSelect.firstElementChild);
+//colorSelect.options.add('Please select a T-shirt theme', 0);
 
 
 

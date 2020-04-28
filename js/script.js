a176188d09edb6275664c4fe6bcf3f6abfedc851
Adrian Collier <30817514+acollier303@users.noreply.github.com>
2020-04-27 22:57:44 -0600

    T-Shirt select -wip

--- js/script.js
+++ js/script.js
@@ -10,4 +10,18 @@ document.getElementById('name').focus();
 //Job role other
 document.getElementById('other-title').style.display='none';
 
+//*** T-Shirt ***
+
+//Hide 'Select Theme' option from 'design' selection
+const designSelect = document.querySelector('#design');
+console.log(designSelect.options[0]);
+
+designSelect.options[0].hidden = 'true';
+
+//Update the “Color” field to read “Please select a T-shirt theme”
+const colorSelect = document.querySelector('#color');
+
+colorSelect.options.add('Please select a T-shirt theme', 0);
+
+
 

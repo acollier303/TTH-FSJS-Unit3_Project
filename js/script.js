d8fcd19b81ce4c3b4f02caccf4eb8a12dd600a9f
Adrian Collier <30817514+acollier303@users.noreply.github.com>
2020-05-02 10:56:24 -0600

    T-Shirt - wip

--- js/script.js
+++ js/script.js
@@ -17,9 +17,7 @@ document.getElementById('other-title').style.display='none';
 T-Shirt Section
 ***/
 
-//Hide 'Select Theme' option from 'design' selection
 const designSelect = document.querySelector('#design');
-designSelect.options[0].hidden = 'true';
 
 //Update the “Color” field to read “Please select a T-shirt theme”
 const colorSelect = document.querySelector('#color');
@@ -30,10 +28,7 @@ for (let i=0; i<colorSelect.length; i+=1){
 //console.log(colorSelect.length);
 const defaultColor = document.createElement('option');
 defaultColor.text = 'Please select a T-shirt theme';
-colorSelect.options.add(defaultColor, 0);
-
-//console.log(colorSelect.firstElementChild);
-//colorSelect.options.add('Please select a T-shirt theme', 0);
+console.log(defaultColor);
 
 
 

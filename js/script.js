583fdb4eaa4631d8bb74f1ef492d111cac478f4a
Adrian Collier <30817514+acollier303@users.noreply.github.com>
2020-05-05 12:23:11 -0600

    T-Shirt section functional

--- js/script.js
+++ js/script.js
@@ -7,16 +7,15 @@ Adrian Collier
 //Focus on first text field on load
 document.getElementById('name').focus();
 
-/***
+/****************
 Job Role Section
-***/
+*****************/
 document.getElementById('other-title').style.display='none';
 
 
-/*** 
+/*************
 T-Shirt Section
-***/
-
+***************/
 const designSelect = document.querySelector('#design');
 const colorSelect = document.querySelector('#color');
 const defaultColor = document.createElement('option');
@@ -26,39 +25,54 @@ defaultColor.text = 'Please select a T-shirt theme';
 colorSelect.appendChild(defaultColor);
 defaultColor.selected = true;
 
+//Hide all options in colorSelect menu
 for (let i=0; i<colorSelect.length; i+=1){
     colorSelect.options[i].hidden = 'true'; 
 }
 
 // Design Menu
-designSelect.options[0].selected = true; //
-
+designSelect.options[0].selected = true; 
 
 const jsPun = designSelect.options[1];
 const loveJs = designSelect.options[2];
 
+// Design Menu Event Listener
 designSelect.addEventListener('change', (e) => {
     designSelect.options[0].hidden = 'true'; // Hides 'Select a theme'
+    colorSelect.options[0].hidden = 'ture';
     
-    const jsPunColors = colorSelect.options.slice(0, 2);
-    console.log(jsPunColors); 
-
+    // jsPun selection
     if (jsPun.selected == true){
-        //alert('jsPun');
         defaultColor.selected = false;
         colorSelect.options[0].selected = true;
-        colorSelect.options[1].hidden ='';
-        colorSelect.options[2].hidden ='';
 
-    }
-
+        //Unhide 'jsPun' options & hide 'loveJs' options
+        for (let i=0; i<colorSelect.length; i+=1){
+            if (i<3){
+                colorSelect.options[i].hidden = '';
+            };
+            
+            if (i>3){
+                colorSelect.options[i].hidden = 'true';
+            };
+        };
+    };
+    
+    // loveJs selection
     if (loveJs.selected == true){
-        defaultColor.selected = false;
         colorSelect.options[3].selected = true;
 
-    }
-
-})
+        //Unhide 'loveJs' options Hide 'jsPun' options
+        for (let i=0; i<colorSelect.length; i+=1){
+            if (i<3){
+                colorSelect.options[i].hidden = 'true';
+            };
+            if (i>3 && i<5){
+                colorSelect.options[i].hidden ='';
+            };
+        };
+    }; 
+});
 
  
 

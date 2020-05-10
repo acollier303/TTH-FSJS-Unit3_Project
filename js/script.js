4a82a2aad3e1464f05e4744d6e28d9c7ebe963b5
Adrian Collier <30817514+acollier303@users.noreply.github.com>
2020-05-09 20:49:38 -0600

    activities wip

--- js/script.js
+++ js/script.js
@@ -80,26 +80,34 @@ designSelect.addEventListener('change', (e) => {
 const checkboxes = document.querySelectorAll('.activities input');
 
 //Add Total input field
-const runningTotal = document.createElement('INPUT');
+let runningTotal = document.createElement('INPUT');
 runningTotal.setAttribute('type', 'text');
 document.querySelector('.activities').appendChild(runningTotal);
 let totalCost = 0;
-runningTotal.setAttribute('value', 'Total: $');
+runningTotal.setAttribute('value', `Total: $ ${totalCost}`);
 
 // Checkbox Event Listener
 document.querySelector('.activities').addEventListener('change', (e) => {
 
     const clicked = e.target;
     const clickedDate = clicked.getAttribute('data-day-and-time'); 
-    const clickedCost = clicked.getAttribute('data-cost');
+    let clickedCost = clicked.getAttribute('data-cost');
+    //clickedCost = parseInt(clickedCost.textContent);
+    console.log(clickedCost.textContent);
+    let totalCost = '';
     
     for (let i=0; i<checkboxes.length; i+=1){
+
         const  checkboxDate = checkboxes[i].getAttribute('data-day-and-time');
         if (clickedDate === checkboxDate && checkboxDate !== checkboxes[i]){
             if (clicked.checked){
                 checkboxes[i].disabled = 'true';
+                totalCost = totalCost + clickedCost;
+                parseInt(totalCost);
+                console.log(totalCost);
             } else {
                 checkboxes[i].disabled = 'false';
+               
             }
         }
     }
